17f57d86ca710e4b577dd63d7ea7d35778d336b5
aroharia <aroharia@gmail.com>
2016-09-27 00:54:42 -0500

    bfs

--- src/assignment3/Main.java
+++ src/assignment3/Main.java
@@ -18,10 +18,14 @@ import java.io.FileNotFoundException;
 import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.HashSet;
-import java.util.List;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Queue;
 import java.util.Scanner;
 import java.util.Set;
+import java.util.Stack;
 
 public class Main {
 	
@@ -53,7 +57,7 @@ public class Main {
 		ArrayList<String> testingBFS = getWordLadderBFS(startWord, endWord);
 		
 		//TO DO
-		//printLadder(testingBFS);
+		printLadder(testingBFS);
 		
 		//TO DO
 		//ArrayList<String> testingDFS = getWordLadderDFS(startWord, endWord);
@@ -71,6 +75,8 @@ public class Main {
 		start_end_wordList = parse(kb);
 		startWord = start_end_wordList.get(0).toString();
 		endWord = start_end_wordList.get(1).toString();
+		
+		
 	}
 	
 	/**
@@ -102,15 +108,95 @@ public class Main {
 		return null; // replace this line later with real return
 	}
 	
+	/**
+	 * Finds the word ladder of 2 words through breadth first search
+	 * @param start is the first word as a String
+	 * @param end is the last word as a String
+	 * @return ArrayList of the ladder or null if it doesn't exist
+	 */
     public static ArrayList<String> getWordLadderBFS(String start, String end) {
-		
-		// TODO some code
+		// dict holds all the words in the given dictionary
 		Set<String> dict = makeDictionary();
-		// TODO more code
 		
-		return null; // replace this line later with real return
+		// to make it comparable to dictionary words
+		start = start.toUpperCase();
+    	end = end.toUpperCase();
+    	
+		// no word ladder if first/last word are not in dictionary
+		if (!(dict.contains(start) && dict.contains(end)))
+			return null;
+    	
+    	// no word ladder if first word = last word
+    	if (start.equals(end))
+			return null;
+		
+		// Queue
+		Queue<String> queue = new LinkedList<String>();
+		queue.add(start);
+		
+		//  map
+		Map<String,String> parentWordMap = new HashMap<String,String>(dict.size());
+		parentWordMap.put(start, null);
+
+		// discovered set
+		Set<String> discovered = new HashSet<String>(dict.size());
+		discovered.add(start);
+		
+		StartSearch : // label for outer loop to break to
+		// while there exists more undiscovered words
+		while (!queue.isEmpty()) {
+			String current = queue.remove();
+			// goes through the word letter by letter (gets length by checking how long the dictionary words are)
+			for (int i=0; i<dict.iterator().next().length(); i++)
+				// checks all letters A-Z
+				for (char j='A'; j<='Z'; j++)
+					// if the letter being 
+					if (j ==current.charAt(i))
+						continue;
+					else {
+						// possible new word
+						String next = current.substring(0,i) + String.valueOf(j) + current.substring(i+1,dict.iterator().next().length());
+						// if not already discovered
+						if (!discovered.contains(next)){
+							// if the newly generated word is the end word, ladder has been found
+							if (next.equals(end)) {
+								parentWordMap.put(end,current);
+								break StartSearch; //breaks out of the outer loop to leave search
+							// if the newly generated word exists, add to queue
+							} else if (dict.contains(next)){
+								queue.add(next);
+								discovered.add(next);
+								parentWordMap.put(next,current);
+							}
+						}
+					}
+		}
+		
+		// return null if ladder does not exist
+		if (!parentWordMap.containsKey(end))
+			return null;
+
+		// stack to get the ladder (in a reversed order)
+		Stack<String> reversedLadder = new Stack<String>();
+		reversedLadder.add(end);
+		String current = end;
+		while ((current = parentWordMap.get(current)) != null){
+			reversedLadder.add(current);
+		}
+		
+		// create a new array list to reverse ladder into
+		ArrayList<String> finalLadder = new ArrayList<String>(reversedLadder.size());
+		while (!reversedLadder.isEmpty()){
+			finalLadder.add(reversedLadder.pop().toLowerCase());
+		}
+		
+		return finalLadder;
 	}
     
+	/**
+	 * Takes all the words from a dictionary text file and converts into a Set<String>
+	 * @return Set of all the words in the given dictionary
+	 */
 	public static Set<String>  makeDictionary () {
 		Set<String> words = new HashSet<String>();
 		Scanner infile = null;
@@ -127,8 +213,12 @@ public class Main {
 		return words;
 	}
 	
+	/**
+	 * Prints the word ladder in the appropriate format
+	 * @param ladder is the ArrayList of words in the ladder, or null if it doens't exist
+	 */
 	public static void printLadder(ArrayList<String> ladder) {
-		
+
 	}
 	// TODO
 	// Other private static methods here
